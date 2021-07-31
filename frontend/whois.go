3f97e5a9b73a8fc79390680f3958ffc3db966b59
Lan Tian <xuyh0120@outlook.com>
2021-07-31 16:56:39 +0800

    frontend: resolve asn in dns/whois/fail order & fix tests

--- frontend/whois.go
+++ frontend/whois.go
@@ -7,6 +7,10 @@ import (
 
 // Send a whois request
 func whois(s string) string {
+	if setting.whoisServer == "" {
+		return ""
+	}
+
 	conn, err := net.Dial("tcp", setting.whoisServer+":43")
 	if err != nil {
 		return err.Error()
